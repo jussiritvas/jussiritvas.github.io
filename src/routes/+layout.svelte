e86124b0d7ea05cf33661876d796b5eac9ffd9f6
qjusrit <jussi.ritvas@meetiqm.com>
2025-12-10 17:58:27 +0200

    update icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import favicon from '$lib/assets/favicon.ico';
-
+	import apple from '$lib/assets/apple-touch-icon.png'
+	import manifest from '$lib/assets/site.webmanifest'
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" href={apple} />
+	<link rel="manifest" href={manifest}>
+
 </svelte:head>
 
 {@render children()}
